800b78534bf7d40d4a30124e28118557ba51be20
Luxusio <44326048+Luxusio@users.noreply.github.com>
2022-12-10 16:13:23 +0900

    build(lib): update test dependencies

--- ADOFAI-Lib/build.gradle.kts
+++ ADOFAI-Lib/build.gradle.kts
@@ -14,10 +14,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-    testImplementation("io.mockk:mockk:1.12.5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("io.mockk:mockk:1.13.2")
 }
 
 tasks.test {
